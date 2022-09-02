e91fc956395e3cff1ab166c5b41bb2cf9761a1fc
almunzir99 <almunzir99@gmail.com>
2022-09-02 21:05:39 +0200

    make header responsive

--- Presentation/BackPanel.WebApplication/ClientApp/src/app/dashboard/components/header/header.component.ts
+++ Presentation/BackPanel.WebApplication/ClientApp/src/app/dashboard/components/header/header.component.ts
@@ -8,10 +8,11 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class HeaderComponent implements OnInit {
   @Output("toggleClick") toggleClickEventEmitter = new EventEmitter<boolean>();
   @Input("toggle") toggle = false;
+  opened = false;
   constructor() { }
 
   ngOnInit(): void {
-  }
+  } 
   onToggle(){
     this.toggle = !this.toggle;
     this.toggleClickEventEmitter.emit(this.toggle);
