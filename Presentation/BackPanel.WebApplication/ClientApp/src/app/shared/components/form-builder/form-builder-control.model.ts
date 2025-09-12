35077d95b41fd5183bd8a71cf4569837317c1a63
almunzir99 <almunzir99@gmail.com>
2025-09-13 00:40:05 +0300

    minor ui fixes

--- Presentation/BackPanel.WebApplication/ClientApp/src/app/shared/components/form-builder/form-builder-control.model.ts
+++ Presentation/BackPanel.WebApplication/ClientApp/src/app/shared/components/form-builder/form-builder-control.model.ts
@@ -5,19 +5,22 @@ import { FormBuilderGroup } from "./form-builder-group.model";
 export class FormBuilderControl {
         title?: string;
         name?: string;
-        icon?:string;
+        icon?: string;
         controlType?: ControlTypes;
-        width?:string = "100%";
-        alignRight?:boolean = false;
-        data?:any[] = [];
-        value?:any | any[]; 
-        validators?:Validators = [];  
+        width?: string = "100%";
+        alignRight?: boolean = false;
+        data?: any[] = [];
+        value?: any | any[];
+        validators?: Validators = [];
         //required for selection
-        isObjectData?:boolean = false;
-        labelProp?:string;
-        valueProp?:string;
+        isObjectData?: boolean = false;
+        labelProp?: string;
+        valueProp?: string;
         // required for table builder
-        controls?:FormBuilderGroup[]
+        controls?: FormBuilderGroup[];
+        disabled?: boolean;
+        extensions?: string;
+        allowMultipleFile?: boolean;
 
 
 } 
--- Presentation/BackPanel.WebApplication/ClientApp/src/app/shared/components/form-builder/form-builder.component.ts
+++ Presentation/BackPanel.WebApplication/ClientApp/src/app/shared/components/form-builder/form-builder.component.ts
@@ -1,5 +1,5 @@
-import {Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
-import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
+import { FormControl, UntypedFormGroup } from '@angular/forms';
 import { FileModel } from 'src/app/core/models/file.models';
 import { ControlTypes } from './control-type.enum';
 import { FormBuilderGroup } from './form-builder-group.model';
@@ -15,7 +15,7 @@ import { TranslateService } from '@ngx-translate/core';
 export class FormBuilderComponent implements OnInit {
   @Input("control-groups") controlsGroups: FormBuilderGroup[] = [];
   @Input("inner-form") innerForm: boolean = false;
-  @Input("title") title?:string = '';
+  @Input("title") title?: string = '';
   @Input('show-cancel-button') showCancelButton = true;
   @Output("formSubmit") submitEventEmitter = new EventEmitter<any>();
   @Output("cancel") cancelEventEmitter = new EventEmitter<void>();
@@ -23,34 +23,31 @@ export class FormBuilderComponent implements OnInit {
   formGroup: UntypedFormGroup = new UntypedFormGroup({});
   controlTypes = ControlTypes;
   direction = 'ltr';
-  constructor(@Inject(MAT_DIALOG_DATA) public data:FormBuilderPropsSpec,private translate:TranslateService) {
-      if(data)
-      {
-        if(data.controlsGroups) this.controlsGroups = data.controlsGroups;
-        this.title = data.title; 
-      }
-      this.direction = this.translate.currentLang == 'ar' ? 'rtl' : 'ltr';
-      this.translate.onLangChange.subscribe((event) => {
-        this.direction = event.lang == 'ar' ? 'rtl' : 'ltr';
-      })
+  constructor(@Inject(MAT_DIALOG_DATA) public data: FormBuilderPropsSpec, private translate: TranslateService) {
+    if (data) {
+      if (data.controlsGroups) this.controlsGroups = data.controlsGroups;
+      this.title = data.title;
+    }
+    this.direction = this.translate.currentLang == 'ar' ? 'rtl' : 'ltr';
+    this.translate.onLangChange.subscribe((event) => {
+      this.direction = event.lang == 'ar' ? 'rtl' : 'ltr';
+    })
   }
   onSubmit() {
     this.submitEventEmitter.emit(this.formGroup.value);
-    if(this.data)
-    {
+    if (this.data) {
       this.data.onSubmit(this.formGroup.value);
     }
   }
-  onCancel(e:any) {
+  onCancel(e: any) {
     e.preventDefault();
     this.cancelEventEmitter.emit();
-    if(this.data)
-    {
-      this.data.onCancel( );
+    if (this.data) {
+      this.data.onCancel();
     }
   }
   // local files Picker Event
-  onFilesPicked(name:string,files:FileModel[]){
+  onFilesPicked(name: string, files: FileModel[]) {
     var target = this.formGroup.controls[name];
     target.setValue(files);
 
@@ -58,18 +55,21 @@ export class FormBuilderComponent implements OnInit {
   ngAfterContentInit() {
     this.controlsGroups.forEach(group => {
       group.controls.forEach(control => {
-        if(control.name)
-        this.formGroup.addControl(control.name, new UntypedFormControl(control.value, control.validators));
+        if (control.name)
+          this.formGroup.addControl(control.name, new FormControl(control.value, control.validators));
       });
     });
   }
   ngOnInit(): void {
   }
-  
+  getFileValue(arg0: any): string | null {
+    return !arg0 ? null : Object.prototype.toString.call(arg0) === "[object String]" ? arg0 : arg0.path;
+  }
 }
+
 export interface FormBuilderPropsSpec {
-  title?:string;
-  controlsGroups:FormBuilderGroup[],
-  onSubmit: (result:any) => void,
-  onCancel:() => void
+  title?: string;
+  controlsGroups: FormBuilderGroup[],
+  onSubmit: (result: any) => void,
+  onCancel: () => void
 }
