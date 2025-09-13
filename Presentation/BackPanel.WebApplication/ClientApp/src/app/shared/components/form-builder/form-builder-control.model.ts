8aac26ad58e6cd1e287e4ca2bd73175a23ec2730
almunzir alhassan <almunzir99@gmail.com>
2025-09-13 22:50:44 +0300

    extends form builder component

--- Presentation/BackPanel.WebApplication/ClientApp/src/app/shared/components/form-builder/form-builder-control.model.ts
+++ Presentation/BackPanel.WebApplication/ClientApp/src/app/shared/components/form-builder/form-builder-control.model.ts
@@ -1,26 +1,41 @@
-import { Validators } from "@angular/forms";
+import { AsyncValidator, FormGroup, Validators } from "@angular/forms";
 import { ControlTypes } from "./control-type.enum";
 import { FormBuilderGroup } from "./form-builder-group.model";
+import { TemplateRef } from "@angular/core";
+import { FormBuilderComponent } from "./form-builder.component";
 
 export class FormBuilderControl {
-        title?: string;
+     title?: string;
         name?: string;
-        icon?: string;
+        icon?:string;
         controlType?: ControlTypes;
-        width?: string = "100%";
-        alignRight?: boolean = false;
-        data?: any[] = [];
-        value?: any | any[];
-        validators?: Validators = [];
+        width?:string = "100%";
+        alignRight?:boolean = false;
+        data?:any[] = [];
+        filterData?:any[] = [];
+        value?:any | any[]; 
+        validators?:Validators | any = [];  
+        asyncValidators?:AsyncValidator | any = [];  
+
+        disabled?:boolean = false;
         //required for selection
-        isObjectData?: boolean = false;
-        labelProp?: string;
-        valueProp?: string;
+        isObjectData?:boolean = false;
+        labelProp?:string;
+        valueProp?:string;
         // required for table builder
-        controls?: FormBuilderGroup[];
-        disabled?: boolean;
-        extensions?: string;
-        allowMultipleFile?: boolean;
+        controls?:FormBuilderGroup[];
+        // required for custom component
+        template?:TemplateRef<any>;
+        allowMultipleFile?:boolean;
+        onChange?:(value:any) => void
+        onLostFocus?:(value:any) => void
+        onChangeWithUpdate?:(value:any,formGroup:FormGroup) => void
+        onChangeWithComponentInfo?:(value:any,formBuilderComponent:FormBuilderComponent) => void
+
+
+
+        displayWith?: ((value: any) => string)|null;
+        extensions?: string
 
 
 } 
--- Presentation/BackPanel.WebApplication/ClientApp/src/app/shared/components/form-builder/form-builder.component.ts
+++ Presentation/BackPanel.WebApplication/ClientApp/src/app/shared/components/form-builder/form-builder.component.ts
@@ -1,74 +1,168 @@
-import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
-import { FormControl, UntypedFormGroup } from '@angular/forms';
+import { Component, EventEmitter, Inject, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FileModel } from 'src/app/core/models/file.models';
 import { ControlTypes } from './control-type.enum';
 import { FormBuilderGroup } from './form-builder-group.model';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilderControl } from './form-builder-control.model';
 import { TranslateService } from '@ngx-translate/core';
-
+import { Direction } from '@angular/cdk/bidi';
+import { MatRadioChange } from '@angular/material/radio';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 @Component({
   selector: 'form-builder',
   templateUrl: './form-builder.component.html',
   standalone: false,
   styleUrls: ['./form-builder.component.scss']
 })
-export class FormBuilderComponent implements OnInit {
+export class FormBuilderComponent implements OnInit, OnChanges {
+
+
   @Input("control-groups") controlsGroups: FormBuilderGroup[] = [];
   @Input("inner-form") innerForm: boolean = false;
   @Input("title") title?: string = '';
+  @Input("confirm-button-text") confirmButtonText?: string = 'Dashboad.Save';
   @Input('show-cancel-button') showCancelButton = true;
+  @Input('show-save-button') showSaveButton = true;
   @Output("formSubmit") submitEventEmitter = new EventEmitter<any>();
   @Output("cancel") cancelEventEmitter = new EventEmitter<void>();
   @Output("tableDelete") tableDeleteEvent = new EventEmitter<any>();
-  formGroup: UntypedFormGroup = new UntypedFormGroup({});
+  @Output("onChangeWithComponentInfo") onChangeWithComponentInfo = new EventEmitter<any>();
+
+  dir: Direction | "auto" = 'rtl'
+  
+  formGroup: FormGroup = new FormGroup({});
   controlTypes = ControlTypes;
-  direction = 'ltr';
-  constructor(@Inject(MAT_DIALOG_DATA) public data: FormBuilderPropsSpec, private translate: TranslateService) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: FormBuilderPropsSpec, @Inject('DIRECTION') public direction: string, private _translateService: TranslateService) {
     if (data) {
       if (data.controlsGroups) this.controlsGroups = data.controlsGroups;
       this.title = data.title;
+      this.confirmButtonText = data.saveButtonText ?? 'Dashboad.Save'
     }
-    this.direction = this.translate.currentLang == 'ar' ? 'rtl' : 'ltr';
-    this.translate.onLangChange.subscribe((event) => {
-      this.direction = event.lang == 'ar' ? 'rtl' : 'ltr';
+    this.dir = _translateService.currentLang == 'ar' ? 'rtl' : 'ltr'
+
+    _translateService.onLangChange.subscribe(res => {
+      this.dir = res.lang == 'ar' ? 'rtl' : 'ltr'
+    }, err => {
+
     })
   }
+
   onSubmit() {
-    this.submitEventEmitter.emit(this.formGroup.value);
+    this.submitEventEmitter.emit(this.formGroup!.getRawValue());
     if (this.data) {
-      this.data.onSubmit(this.formGroup.value);
+      this.data.onSubmit(this.formGroup!.getRawValue());
     }
   }
   onCancel(e: any) {
     e.preventDefault();
     this.cancelEventEmitter.emit();
+    console.log(this.formGroup);
     if (this.data) {
       this.data.onCancel();
     }
   }
   // local files Picker Event
   onFilesPicked(name: string, files: FileModel[]) {
-    var target = this.formGroup.controls[name];
+    var target = this.formGroup!.controls[name];
     target.setValue(files);
 
   }
-  ngAfterContentInit() {
-    this.controlsGroups.forEach(group => {
+  ngOnInit(): void {
+    this.formGroup = new FormGroup({});
+    this.controlsGroups.filter(x => !x.hidden).forEach(group => {
       group.controls.forEach(control => {
         if (control.name)
-          this.formGroup.addControl(control.name, new FormControl(control.value, control.validators));
+          this.formGroup!.addControl(control.name, new FormControl({ value: control.value, disabled: control.disabled }, control.validators, control.asyncValidators));
+
       });
+      if (group.Validators)
+        group.Validators.forEach(val => {
+          this.formGroup?.addValidators(val)
+        });
     });
   }
-  ngOnInit(): void {
+  ngAfterViewInit() {
   }
+  onSelectionValueChanged($event: any, control: FormBuilderControl) {
+    if (control.onChange) {
+      control.onChange($event);
+      // update componen
+    }
+    if (control.onChangeWithUpdate) {
+      control.onChangeWithUpdate($event, this.formGroup!)
+    }
+    if (control.onChangeWithComponentInfo) {
+      control.onChangeWithComponentInfo($event, this)
+    }
+  }
+  onInput($event: any, control: FormBuilderControl) {
+    console.log($event)
+    if (control.onChange) {
+      control.onChange($event.target.value);
+      // update component
+
+    }
+    if (control.onChangeWithUpdate) {
+      control.onChangeWithUpdate($event.target.value, this.formGroup!)
+    }
+  }
+  onDateChange($event: MatDatepickerInputEvent<any, any>, control: FormBuilderControl) {
+    var value = $event.value;
+    if (control.onChange) {
+      control.onChange(value);
+      // update component
+
+    }
+    if (control.onChangeWithUpdate) {
+      control.onChangeWithUpdate(value, this.formGroup!)
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // check changes on controlsGroups
+    if (changes.controlsGroups) {
+      this.controlsGroups = changes.controlsGroups.currentValue;
+      this.formGroup = new FormGroup({});
+      this.controlsGroups.filter(c => !c.hidden).forEach(group => {
+        group.controls.forEach(control => {
+          if (control.name)
+            this.formGroup!.addControl(control.name, new FormControl({ value: control.value, disabled: control.disabled }, control.controlType == ControlTypes.Hidden ? [] : control.validators));
+        });
+      });
+    }
+  }
+  onRadioChanged($event: MatRadioChange, control: FormBuilderControl) {
+    if (control.onChangeWithUpdate) {
+      control.onChangeWithUpdate($event.value, this.formGroup!)
+    }
+    if (control.onChangeWithComponentInfo) {
+      control.onChangeWithComponentInfo($event.value, this)
+    }
+
+  }
+
   getFileValue(arg0: any): string | null {
     return !arg0 ? null : Object.prototype.toString.call(arg0) === "[object String]" ? arg0 : arg0.path;
   }
+  onLostFocus($event: any, control: FormBuilderControl) {
+    if (control.onLostFocus) {
+      control.onLostFocus($event.target.value);
+      // update component
+
+    }
+  }
+  onAutoCompleteChanged($event: any, control: FormBuilderControl) {
+    if (control.onChange)
+      control.onChange($event.target.value)
+    control.filterData = control.data?.filter(c => c.includes($event.target.value))
+  }
 }
+
 
 export interface FormBuilderPropsSpec {
   title?: string;
+  saveButtonText?: string | null;
   controlsGroups: FormBuilderGroup[],
   onSubmit: (result: any) => void,
   onCancel: () => void
